40a891b8cfea8a844349ab258d8c1a9aba58cae3
Daniel Katz <Daniel@daniels-macbook-pro.local>
2016-04-28 17:25:57 -0400

    Initial Commit

---
+++ SagamoreApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SagamoreApp
+//
+//  Created by Daniel Katz on 4/28/16.
+//  Copyright © 2016 Stratton Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
